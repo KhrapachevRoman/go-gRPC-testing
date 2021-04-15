b4363e45c96a682f9b9d5428ab47232d223e1e2c
KhrapachevRoman <mr.khrapachev@gmail.com>
2021-04-15 13:26:39 +1000

    refactoring grpc server

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"os"
 
+	"github.com/KhrapachevRoman/currency/data"
 	protos "github.com/KhrapachevRoman/currency/protos/currency"
 	"github.com/KhrapachevRoman/currency/server"
 	"google.golang.org/grpc"
@@ -15,16 +17,31 @@ func main() {
 
 	log := log.New(os.Stdout, "currency-api", log.LstdFlags)
 
+	// generate a new ExchangeRates
+	rates, err := data.NewRates(log)
+	if err != nil {
+		log.Fatal("Unable to generate rates, error", err)
+		os.Exit(1)
+	}
+	// create a new gRPC server, use WithInsecure to allow http connections
 	gs := grpc.NewServer()
-	cs := server.NewCurrency(log)
+
+	// create an instanse of the Currency server
+	cs := server.NewCurrency(rates, log)
+
+	// register the currency server
 	protos.RegisterCurrencyServer(gs, cs)
 
+	// register the reflection service which allows clients to determine the methods
+	// for this gRPC service
 	reflection.Register(gs)
 
-	l, err := net.Listen("tcp", ":9092")
+	// create a TCP socket for inbound server connections
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", 9092))
 	if err != nil {
 		log.Fatal("Unable to listen", "error", err)
 		os.Exit(1)
 	}
+	// listen for requests
 	gs.Serve(l)
 }
--- server/currency.go
+++ server/currency.go
@@ -4,24 +4,29 @@ import (
 	"context"
 	"log"
 
+	"github.com/KhrapachevRoman/currency/data"
 	protos "github.com/KhrapachevRoman/currency/protos/currency"
 )
 
 // Currency is a gRPC server it implements the methods defined by the CurrencyServer interface
 type Currency struct {
-	l *log.Logger
+	rates *data.ExchangeRates
+	log   *log.Logger
 	protos.UnimplementedCurrencyServer
 }
 
 // NewCurrency creates a new Currency server
-func NewCurrency(log *log.Logger) *Currency {
-	return &Currency{l: log}
+func NewCurrency(r *data.ExchangeRates, l *log.Logger) *Currency {
+	return &Currency{rates: r, log: l}
 }
 
 // GetRate implements the CurrencyServer GetRate method and returns the currency exchange rate
 // for the two given currencies.
 func (c *Currency) GetRate(ctx context.Context, in *protos.RateRequest) (*protos.RateResponse, error) {
-	c.l.Println("Handle GetRate", "base", in.GetBase(), "destination", in.GetDestination())
-
-	return &protos.RateResponse{Rate: 0.5}, nil
+	c.log.Println("Handle GetRate", "base", in.GetBase(), "destination", in.GetDestination())
+	rate, err := c.rates.GetRate(in.GetBase().String(), in.GetDestination().String())
+	if err != nil {
+		return nil, err
+	}
+	return &protos.RateResponse{Rate: rate}, nil
 }
